ce8ee9bb903c71925d6b49bae37ce7aeae5796bb
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-12-14 19:20:50 +0100

    add ng zorro to have the modal

--- projects/ortiz-tuna/src/app/app.module.ts
+++ projects/ortiz-tuna/src/app/app.module.ts
@@ -5,6 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {TunaGroupComponent} from './tuna-group.component';
 import {AnchoviesGroupComponent} from './anchovies-group.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 
 @NgModule({
@@ -15,9 +24,12 @@ import {AnchoviesGroupComponent} from './anchovies-group.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
