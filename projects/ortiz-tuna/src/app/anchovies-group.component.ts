676b8e030164afae57e3c5e3ee582a1d8961712e
Grace Henriquez <graciela.fernandez@businesspartner.roche.com>
2020-12-16 22:07:27 +0100

    replace last tiles

--- projects/ortiz-tuna/src/app/anchovies-group.component.ts
+++ projects/ortiz-tuna/src/app/anchovies-group.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {TimelineMax} from 'gsap';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'ot-anchovies-group',
   templateUrl: './anchovies-group.component.html',
   styleUrls: ['./anchovies-group.component.scss']
 })
-export class AnchoviesGroupComponent implements OnInit {
+export class AnchoviesGroupComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+
+  // @ts-ignore
+  @ViewChild('anchoviesContainer', {static: true}) anchoviesContainer: ElementRef<HTMLCanvasElement>;
+
+  // @ts-ignore
+  timeline: TimelineMax;
+
+  pixiApp: unknown;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+
+
+  }
+
+  ngAfterContentInit(): void {
+
+    this.pixiApp = new PIXI.Application({
+      view: document.querySelector('#anchoviesContainer') as HTMLCanvasElement,
+      width: 568,
+      height: 268,
+      backgroundColor: 0x000000,
+      antialias: true
+    });
+
+    const sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+
+    sprite.width = sprite.height = 200;
+    sprite.position.set(100);
+    // @ts-ignore
+    this.pixiApp.stage.addChild(sprite);
+
+    this.timeline = new TimelineMax({repeat: -1, yoyo: true})
+      .to(sprite, 1, {pixi: {tint: '#ff0'}})
+      .to(sprite, 1, {pixi: {tint: 0x2196F3}})
+      .to(sprite, 1, {pixi: {tint: 'hsl(180, 90%, 60%)'}})
+      .to(sprite, 1, {pixi: {tint: 'rgb(236, 64, 122)'}});
   }
 
 }
--- projects/ortiz-tuna/src/app/fade-in-animation.directive.ts
+++ projects/ortiz-tuna/src/app/fade-in-animation.directive.ts
@@ -1,20 +1,42 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
-import {CoreAnimationDirective} from './core-animation.directive';
+import {Directive, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {TimelineMax} from 'gsap';
 
 @Directive({
   selector: '[otFadeInAnimation]'
 })
-export class FadeInAnimationDirective extends CoreAnimationDirective implements OnInit {
+export class FadeInAnimationDirective implements OnInit {
+
+  @Input() duration = 1;
+  @Input() delay = 0;
+
+  // tslint:disable-next-line:no-output-native
+  @Output() complete: EventEmitter<void> = new EventEmitter();
+  @Output() reverseComplete: EventEmitter<void> = new EventEmitter();
+  /**
+   * @link https://greensock.com/docs/v2/TimelineMax
+   */
+  protected timeline: TimelineMax;
+
   constructor(protected element: ElementRef) {
-    super(element);
+    this.timeline = new TimelineMax({
+      onComplete: (_: any) => this.complete.emit(),
+      onReverseComplete: (_: any) => this.reverseComplete.emit(),
+      paused: true,
+      reversed: true
+    });
   }
 
   ngOnInit(): void {
     this.animateIn();
   }
 
-  protected animateIn(): void {
+  animateIn(): void {
     this.timeline.from(this.element.nativeElement, this.duration, {opacity: '0', ease: 'Expo.easeInOut'}, this.delay);
-    super.animateIn();
+
+    if (this.timeline.isActive()) {
+      this.timeline.kill();
+    }
+    this.timeline.play();
   }
+
 }
